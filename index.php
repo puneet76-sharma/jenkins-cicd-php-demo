5779836aaa4ff22bc98c37271aadac6eaa77a882
Puneet Sharma <69373560+puneet76-sharma@users.noreply.github.com>
2022-09-01 16:50:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP... done</p>'; ?> 
+ <?php echo '<p>Hello world from PHP... Jenkins Pipeline </p>'; ?> 
  </body>
 </html>
